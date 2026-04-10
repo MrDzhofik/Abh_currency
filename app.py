18ad120e0840018e2fcf1bc1aad981fda0f23714
MrDzhofik <djafarovemil04@mail.ru>
2026-04-11 00:31:17 +0300

    Add timeout exception handler

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dotenv
 from datetime import time
 from telegram.ext import ApplicationBuilder, CommandHandler
 from handlers.bot import start, get, send_daily_rates
+from telegram.request import HTTPXRequest
 
 PORT = int(os.environ.get("PORT", 10000))
 
@@ -10,9 +11,14 @@ dotenv.load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+request = HTTPXRequest(
+    connect_timeout=20,
+    read_timeout=20
+)
+
 
 def main():
-    application = ApplicationBuilder().token(TOKEN).build()
+    application = ApplicationBuilder().token(TOKEN).request(request).build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("get", get))
--- handlers/bot.py
+++ handlers/bot.py
@@ -1,5 +1,7 @@
 from telegram import Update
 from telegram.ext import ContextTypes
+from telegram.error import TimedOut
+import asyncio
 
 from services.format import build_rates_message
 
@@ -30,11 +32,15 @@ async def get(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def send_daily_rates(context: ContextTypes.DEFAULT_TYPE):
-
     text = build_rates_message()
 
-    await context.bot.send_message(
-        chat_id=CHAT,
-        text=text,
-        parse_mode="Markdown"
-    )
+    for i in range(3):
+        try:
+            return await context.bot.send_message(
+                chat_id=CHAT,
+                text=text,
+                parse_mode="Markdown"
+            )
+        except TimedOut:
+            print(f"Timeout, retry {i+1}")
+            await asyncio.sleep(2)
